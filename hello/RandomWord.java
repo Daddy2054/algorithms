d5b576b99d22511b1c2dd9656b9a2ab5a5da9853
Jean <acalto2@gmail.com>
2024-06-19 22:19:34 +0200

    renaming class

--- hello/RandomWord.java
+++ hello/RandomWord.java
@@ -8,7 +8,7 @@ import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.StdRandom;
 
-public class RandomWorld {
+public class RandomWord {
     public static void main(String[] args) {
         int x = 0;
         String result = "";
