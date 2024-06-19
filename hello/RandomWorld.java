9dc4a7b87a5b27847d31f87081a08d81e3d20b90
Jean <acalto2@gmail.com>
2024-06-19 21:57:29 +0200

    1st assignment hello

---
+++ hello/RandomWorld.java
@@ -0,0 +1,25 @@
+/* *****************************************************************************
+ *  Name:              Ada Lovelace
+ *  Coursera User ID:  123456
+ *  Last modified:     October 16, 1842
+ **************************************************************************** */
+
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+
+public class RandomWorld {
+    public static void main(String[] args) {
+        int x = 0;
+        String result = "";
+        String nextWord;
+        while (!StdIn.isEmpty()) {
+            nextWord = StdIn.readString();
+            x++;
+            if (StdRandom.bernoulli(1.0 / x)) {
+                result = nextWord;
+            }
+        }
+        StdOut.println(result);
+    }
+}
